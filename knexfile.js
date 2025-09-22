08f516d6ce3a762d93b9e094fa7b7b26a37614ce
DECLEONES <decelones.souza@aluno.senai.br>
2025-09-22 19:30:09 -0400

    Adiciona configuração de produção ao knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,3 @@
-// knexfile.js
 require('dotenv').config();
 
 module.exports = {
@@ -9,7 +8,19 @@ module.exports = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/db/migrations'
+    }
+  },
+
+  // --- SECÇÃO NOVA E IMPORTANTE ---
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false } // Necessário para conexões na Render
     },
     migrations: {
       directory: './src/db/migrations'
