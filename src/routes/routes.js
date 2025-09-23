3e4aa958c236417e3ce9595ba0d54278a58f0bb2
DECLEONES <decelones.souza@aluno.senai.br>
2025-09-23 15:33:57 -0400

    Adiciona controller e rotas para o controlo de vacinas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,7 @@ const DashboardController = require('../controllers/DashboardController');
 const AnimalController = require('../controllers/AnimalController');
 const WeightController = require('../controllers/WeightController');
 const EventController = require('../controllers/EventController');
+const VaccineController = require('../controllers/VaccineController');
 
 // Middlewares
 const authMiddleware = require('../middlewares/auth.js');
@@ -31,6 +32,13 @@ routes.delete('/animals/:id', permit(['ADMIN']), AnimalController.delete);
 
 routes.get('/animals/:animal_id/weights', WeightController.index);
 routes.post('/animals/:animal_id/weights', permit(['ADMIN', 'FUNCIONARIO']), WeightController.create);
+
+// --- Rotas de Vacinas ---
+routes.get('/animals/:animal_id/vaccines', VaccineController.index);
+routes.post('/animals/:animal_id/vaccines', permit(['ADMIN', 'FUNCIONARIO']), VaccineController.create);
+
+routes.get('/events', EventController.index);
+// ...
 
 routes.get('/events', EventController.index);
 routes.post('/events', permit(['ADMIN', 'FUNCIONARIO']), EventController.create);
